7109a9b640a6838cf14f8f32462d63f2f5339f9e
mdiazpj <manolo.diaz@pjchile.com>
2024-07-29 13:02:56 -0400

    some-fixes

--- src/main/java/com/backend/BackendJWT/Models/Auth/User.java
+++ src/main/java/com/backend/BackendJWT/Models/Auth/User.java
@@ -9,11 +9,13 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
+
+
 
 @Builder
 @NoArgsConstructor
